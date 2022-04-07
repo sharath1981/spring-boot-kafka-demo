fe1fa8a38b6cb3b88e6b0e78dacbb51377c3e306
SHARATH KUMAR B <sharath1981@gmail.com>
2022-04-07 12:33:20 +0530

    Updated

--- src/main/java/com/ryana/SpringBootKafkaDemoApplication.java
+++ src/main/java/com/ryana/SpringBootKafkaDemoApplication.java
@@ -1,5 +1,7 @@
 package com.ryana;
 
+import java.util.stream.IntStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -17,19 +19,27 @@ import lombok.extern.log4j.Log4j2;
 @SpringBootApplication
 public class SpringBootKafkaDemoApplication {
 
+	private static final String TOPIC = "topic_1";
+	private static final String GROUP_1 = "group_1";
+	private static final String GROUP_2 = "group_2";
+
 	private final KafkaTemplate<String, Object> template;
-	private static final String TOPIC = "my_topic";
-	private static final String PARTITION = "my_topic-0";
 
 	@GetMapping("/publish/{message}")
 	public String publish(@PathVariable final String message) {
-		template.send(TOPIC, message);
-		return "Message Sent => "+message;
+		IntStream.rangeClosed(1, 10)
+				.forEach(i -> template.send(TOPIC, message + i));
+		return "Message Sent => " + message;
 	}
 
-	@KafkaListener(topics = TOPIC, groupId = PARTITION)
-	public void listener(final String message) {
-		log.info("@KafkaListener Received message {}", message);
+	@KafkaListener(topics = TOPIC, groupId = GROUP_1)
+	public void listener1(final String message) {
+		log.info("@KafkaListener-1 Received message {}", message);
+	}
+
+	@KafkaListener(topics = TOPIC, groupId = GROUP_2)
+	public void listener2(final String message) {
+		log.info("@KafkaListener-2 Received message {}", message);
 	}
 
 	public static void main(String[] args) {
